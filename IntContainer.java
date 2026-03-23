6c547cd031114a6a3805fae6150ba88f12f65651
leandoero <seventeen.iinc@gmail.com>
2026-03-23 13:49:44 +0300

    added add and get methods

--- IntContainer.java
+++ IntContainer.java
@@ -18,4 +18,59 @@ public class IntContainer {
         head = null;
         size = 0;
     }
+
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void add(int index, int value) {
+        checkIndexForAdd(index);
+        Node newNode = new Node(value);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node prev = getNode(index - 1);
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+        size++;
+    }
+
+    public int get(int index) {
+        checkIndex(index);
+        return getNode(index).value;
+    }
+
+    private Node getNode(int index) {
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(
+                "Индекс " + index + " выходит за пределы. Размер: " + size);
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(
+                "Индекс " + index + " выходит за пределы. Размер: " + size);
+        }
+    }
 }
